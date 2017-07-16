19d2d327f6224657d427203c05586a1fff676523
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-07-16 20:25:23 +0700

    Create drc.js

---
+++ javascript/drc.js
@@ -0,0 +1,6 @@
+$(document).ready(function () { 
+if ($("#danilrafiqi,.danilrafiqi").attr("href") != "http://www.danilrafiqi.xyz/") {
+        window.location.href = "http://www.danilrafiqi.xyz"; };
+if($("#danilrafiqi")["length"]==0){window["location"]="http://www.danilrafiqi.xyz";};
+document.getElementById('danilrafiqi').innerHTML="<a href='http://www.danilrafiqi.xyz' target='_blank'>Danil Rafiqi</a>";
+});
