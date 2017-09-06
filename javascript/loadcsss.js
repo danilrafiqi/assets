24d9987131fbeda8134e40b974d56f5e3a7fa48d
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-09-06 22:34:59 +0700

    Create loadcsss.js

---
+++ javascript/loadcsss.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>
+//<![CDATA[
+//CSS 
+(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("LINK HOST.CSS");
+loadCSS("LINK HOST.CSS");
+loadCSS("LINK HOST.CSS");
+//Javascript 
+function loadScript(src) {var element = document.createElement("script");element.src = src;document.body.appendChild(element);}function downloadJSAtOnload() {
+loadScript("LINK HOST.js");
+loadScript("LINK HOST.js"); 
+loadScript("LINK HOST.js"); 
+}
+if (window.addEventListener)window.addEventListener("load", downloadJSAtOnload, false);else if (window.attachEvent)window.attachEvent("onload", downloadJSAtOnload);else window.onload = downloadJSAtOnload;
+</script>
